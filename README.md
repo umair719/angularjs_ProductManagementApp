33cbff4f84ccaed58ded7fe86fae78ef1ec76d9f
Umair Khalid <umair719@gmail.com>
2014-10-11 22:57:45 -0500

    Setup of UI Router and States

--- README.md
+++ README.md
@@ -5,5 +5,20 @@ Full featured Product Management App
 
 Written in AngularJS
 
-From PluralSight AngularJS Advance Training
+From [PluralSight AngularJS Line of Business Applications](http://www.pluralsight.com/courses/angularjs-line-of-business-applications) Course
+
+### Site Map
+
+    Menu -> Welcome Page
+    Menu -> Product List -> Product Detail
+            Product List -> Product Edit
+    Menu -> Product Edit    
+    
+### State names for each view in the Angular-UI Router:
+
+* Welcome Page : home
+* Product List : productList
+* Product Detail : productDetail
+* Product Edit : productEdit
+
 
--- app/app.js
+++ app/app.js
@@ -4,8 +4,21 @@
 
 (function () {
     "use strict";
-    var app = angular.module("productManagement", ["common.services", "productResourceMock"]);
+    var app = angular.module("productManagement",
+        ["common.services", "ui.router", "productResourceMock"]);
 
+    app.config(["$stateProvider", "$urlRouterProvider",
+            function ($stateProvider, $urlRouterProvider) {
+
+                $urlRouterProvider.otherwise("/products");
+
+                $stateProvider.state("productList", {
+                    url: "/products",
+                    templateUrl: "app/products/productListView.html",
+                    controller: "ProductListCtrl as vm"
+                })
+            }]
+    );
 }());
 
 
